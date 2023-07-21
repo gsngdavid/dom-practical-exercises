239081cf736af3b971d747f8a74bb5968994b0fb
gsngdavid <gsngdavid@gmail.com>
2023-07-21 14:15:16 +0200

    feat: Decrement the counter every one second

--- Simple countdown/main.js
+++ Simple countdown/main.js
@@ -1,3 +1,16 @@
+let counter = 10;
+
 // Get elements
-const counter = document.getElementById('counter');
+const timer = document.getElementById('timer');
 const actions = document.getElementById('actions');
+
+// Functions
+
+const timerChange = setInterval (() => {
+    if(counter === 1) {
+        actions.remove();
+        timer.textContent = 0;
+        return clearInterval(timerChange);
+    }
+    timer.textContent = --counter;
+} , 1000);
