99d6cdef96089eddb8bd64953907ab0899f687f3
gsngdavid <gsngdavid@gmail.com>
2023-07-21 12:00:59 +0200

    feat: Add items to the list

--- Todo App/main.js
+++ Todo App/main.js
@@ -1,4 +1,20 @@
+const todoList = [];
+
+// Get elements
+
 const form = document.getElementById('form');
 const list = document.getElementById('todo-list');
 
 
+// Functions
+
+function addItemHandler(e) {
+    e.preventDefault();
+    const item = form.todo.value.trim();
+    if(item.length === 0 || todoList.includes(item)) return;
+    todoList.push(item);
+}
+
+// Attach event handlers
+
+form.addEventListener('submit', addItemHandler);
